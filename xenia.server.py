b0f77fb0255bb2d81c6b6abaf00bac532aa0d4ee
Oskar Freye <kontakt@inf-o.eu>
2018-02-16 09:37:33 +0000

    Fixed Unicode

--- xenia.server.py
+++ xenia.server.py
@@ -1,13 +1,14 @@
+# -*- coding: utf-8 -*-
 import json
 from io import StringIO
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
+from unidecode import unidecode
+from urllib.parse import unquote
 
 
 class Xenia:
     def __init__(self):
-        print("[X]> Welcome, I'm Xenia. How can I help you?")
-        
+        self.imgUrl = "https://png.icons8.com/color/96/000000/bavarian-girl.png"
         self.brain = open("memory/xenia.brain", 'r').readlines()
         self.answers = open("memory/answer.brain", 'r').readlines()
         self.answer = ""
@@ -67,8 +68,6 @@ class Xenia:
             score = 0
         self.winner = highscore[1]
 
-        print(str(self.classify) + " " + str(self.keywords))
-
         ctk, ending = self.check_to_add_keywords()
         if ctk:
             for keyword in self.keywords:
@@ -92,14 +91,14 @@ class Xenia:
         io = StringIO()
         if "s" in self.classify or ("k" in self.classify and len(self.classify) == 1 and len(self.keywords) != None):
             url = "https://www.google.de/search?q="+self.query.replace(" ", "%20")
-            json.dump({"answer": answer, "classify": "s", "url": url}, io)
+            json.dump({"answer": answer, "classify": "s", "url": url, "img": self.imgUrl, "position": "left"}, io)
             return io.getvalue()
         elif "m" in self.classify:
             url = "https://www.google.de/maps/search/"+self.query.replace(" ", "%20")
-            json.dump({"answer": answer, "classify": "m", "url": url},io)
+            json.dump({"answer": answer, "classify": "m", "url": url, "img": self.imgUrl, "position": "left"},io)
             return io.getvalue()
         else:
-            json.dump({"answer": answer},io)
+            json.dump({"answer": answer, "img": self.imgUrl, "position": "left"},io)
             return io.getvalue()
 
     def clean(self):
@@ -114,21 +113,25 @@ class Xenia:
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
             
-        request_path = self.path
-        
+        request_path = unquote(self.path, encoding="utf-8")
+ 
         print(request_path)
-        
-        request_path = request_path.strip("/").replace("%20", " ")
-        xenia = Xenia()
-        xenia.handleInput(request_path)
-        answer = xenia.answerRequest()
-        print(answer)
-        response = bytes(answer, 'utf-8')
-        xenia.clean()
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        self.wfile.write(response)
+        if("favicon.ico" in request_path):
+            self.send_response(0)
+        else:
+            request_path = request_path.strip("/").replace("%20", " ")
+            xenia = Xenia()
+            xenia.handleInput(request_path)
+            answer = xenia.answerRequest()
+            print(answer)
+            response = bytes(answer, 'utf-8')
+            print(response)
+            xenia.clean()
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json; charset=UTF-8')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.wfile.write(response)
         
 
 
@@ -136,7 +139,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 
 def main():
-    port = 8080
+    port = 80
     print('Listening on localhost:%s' % port)
     server = HTTPServer(('', port), RequestHandler)
     server.serve_forever()
